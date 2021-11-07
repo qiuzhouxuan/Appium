b987f040ace3d1d46ba0377e27717f83ca3b5bf6
15527114645@163.com <15527114645@163.com>
2021-11-07 22:47:19 +0800

    ceshi

--- test_appium_5.py
+++ test_appium_5.py
@@ -1,6 +1,10 @@
 import pytest
 from appium import webdriver
 import logging
+
+from appium.webdriver.common.touch_action import TouchAction
+
+
 class TestDW():
     def setup(self):
         desired_caps = {}
@@ -36,33 +40,36 @@ class TestDW():
     #     self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name'and@text='阿里巴巴']").click()
     #     current_price= float(self.driver.find_element_by_id("com.xueqiu.android:id/current_price").text)
     #     assert current_price>100
-    def test_attr(self):
-        """
-        打开雪球应用
-        定位首页的搜索框
-        判断搜索框是否可用，并查看name属性
-        打印搜索框这个元素的左上角坐标和他的宽度
-        向搜索框输入：alibaba
-        判断阿里巴巴是否可见
-        如果可见打印搜索成功，不可见打印搜索失败
-        :return:
-        """
-        element = self.driver.find_element_by_id("com.xueqiu.android:id/tv_search")
-        search_enabled = element.is_enabled()
-        print(element.text)
-        print(element.location)
-        print(element.size)
-        if search_enabled == True:
-            element.click()
-            self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("alibaba")
-            alibaba_element = self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name'and@text='阿里巴巴']")
-            # alibaba_element.is_displayed()
-            # 判断元素是否可见
-            element_display = alibaba_element.get_attribute("displayed")
-            if element_display =="true":
-                print("搜索成功")
-            else:
-                print("搜索失败")
+    # def test_attr(self):
+    #     """
+    #     打开雪球应用
+    #     定位首页的搜索框
+    #     判断搜索框是否可用，并查看name属性
+    #     打印搜索框这个元素的左上角坐标和他的宽度
+    #     向搜索框输入：alibaba
+    #     判断阿里巴巴是否可见
+    #     如果可见打印搜索成功，不可见打印搜索失败
+    #     :return:
+    #     """
+    #     element = self.driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+    #     search_enabled = element.is_enabled()
+    #     print(element.text)
+    #     print(element.location)
+    #     print(element.size)
+    #     if search_enabled == True:
+    #         element.click()
+    #         self.driver.find_element_by_id("com.xueqiu.android:id/search_input_text").send_keys("alibaba")
+    #         alibaba_element = self.driver.find_element_by_xpath("//*[@resource-id='com.xueqiu.android:id/name'and@text='阿里巴巴']")
+    #         # alibaba_element.is_displayed()
+    #         # 判断元素是否可见
+    #         element_display = alibaba_element.get_attribute("displayed")
+    #         if element_display =="true":
+    #             print("搜索成功")
+    #         else:
+    #             print("搜索失败")
+    def test_touchaction(self):
+        action = TouchAction(self.driver)
+        action.press(x=344,y=1097).wait(200).move_to(x=731,y=463).release().perform()
 
 
 if __name__ == '__main__':
